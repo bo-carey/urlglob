494966b95642b0d4fc3862e15a7540d5081bf175
jelly <bo.carey@outlook.com>
2021-10-30 19:24:35 -0400

    Updated variable names in test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,8 @@ import UrlGlob from "./index";
 
 describe('UrlGlob class', () => {
 	it('should match url glob patterns', () => {
-		const u = new UrlGlob('*w3schools*/*d/**');
-		expect(u.match('https://www.w3schools.com/md/sdfsdf/dfgdfg/')).toBe(true);
-		expect(u.match('https://www.google.com/md/sdfsdf/dfgdfg/')).toBe(false);
-	})
+		const glob = new UrlGlob('*w3schools*/*d/**');
+		expect(glob.match('https://www.w3schools.com/md/sdfsdf/dfgdfg/')).toBe(true);
+		expect(glob.match('https://www.google.com/md/sdfsdf/dfgdfg/')).toBe(false);
+	});
 })
